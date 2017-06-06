c8ccf21e57d619fc0fcf1e035000617ef2be3029
peyman valikhanli <peymanvalikhanli2012@gmail.com>
2017-06-06 10:27:19 +0430

    upload project

---
+++ admin_panel/controller/index.php
@@ -0,0 +1,314 @@
+<?php
+
+
+//include "API.php";
+
+require_once '../model/data_access/access.php';
+require_once '../model/data_access/lang.php';
+
+session_start();
+if (!isset($_REQUEST["act"])) {
+    exit;
+}
+switch ($_REQUEST["act"]) {
+
+    case 'logout':
+        try {
+
+            $_SESSION = null;
+            session_destroy();
+        } catch (Exception $e) {
+        }
+        send_result(array('act' => 'logout'));
+        break;
+    case 'register':
+        $f = array('email', 'pass', 'avatar', 'username');
+        $valid_data = check_validation($f);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $res = access::set_user($_REQUEST['email'], $_REQUEST['pass'], $_REQUEST['avatar'], $_REQUEST['username']);
+        if (is_numeric($res)) {
+            $_SESSION['user'] = access::get_user_by_email_pass($_REQUEST['email'], $_REQUEST['pass']);
+            send_result(array('Result' => 'profile.html', 'act' => 'location'));
+            exit;
+        } else {
+            send_msg(lang::$last_registered_data, lang::$error);
+            exit;
+        }
+        break;
+    case 'login':
+        //session_start();
+        $f = array('email', 'pass');
+        $valid_data = check_validation($f);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $_SESSION['user'] = access::get_user_by_email_pass($_REQUEST['email'], $_REQUEST['pass']);
+        if (isset($_SESSION['user']) && isset($_SESSION['user'][0]['email'])) {//ß== $_REQUEST['email'])){
+            access::set_user_login($_SESSION['user'][0]['id']);
+            send_result(array('Result' => 'index.html', 'act' => 'location'));
+            exit;
+        } else {
+            send_msg(lang::$is_not_login, lang::$error);
+            exit;
+        }
+
+        break;
+    case 'edit_profile':
+
+        $f = array('username', 'name', 'email', 'cart_no', 'shaba_no', 'sex', 'address');
+        $valid_data = check_validation($f);
+
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+
+        $res = access::edit_user($_SESSION['user'][0]['id'], $_REQUEST['username'], $_REQUEST['name'], $_REQUEST['sex'], $_REQUEST['address'], $_REQUEST['email'], $_REQUEST['cart_no'], $_REQUEST['shaba_no']);
+        $_SESSION['user'] = access::get_user_by_email_pass($_SESSION['user'][0]['email'], $_SESSION['user'][0]['password']);
+        if ($res == 1) {
+            send_result(array('Result' => 'Home.html', 'act' => 'location'));
+            exit;
+        } else {
+            send_msg(lang::$is_not_set_profile, lang::$error);
+            extit;
+        }
+
+        break;
+    case 'get_user':
+        //session_start();
+        if (!isset($_SESSION['user'][0]['email'])) {
+            send_result(array('Result' => 'login.html', 'act' => 'location'));
+            exit;
+        }
+        $_SESSION['user'][0]['act'] = 'get_user';
+        send_result($_SESSION['user'][0]);
+        break;
+    case 'get_user_by_username' :
+        $res = access::get_user_by_username_or_email($_REQUEST['user']);
+        $res['act']='get_user_by_username';
+        send_result($res);
+        break;
+    case 'get_all_roles':
+        send_result(access::get_all_role());
+        break;
+    case 'get_role_by_id':
+        $valid_data = check_validation(array("id"));
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $res = access::get_role_by_id($_REQUEST['id']);
+        send_result($res);
+        break;
+    case 'get_userrole_by_userid':
+        $valid_data = check_validation(array("id"));
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $res = access::get_user_role_by_user_id($_REQUEST['id']);
+        send_result($res);
+        break;
+    case 'set_role':
+        $arr = array("role", "createby");
+        $valid_data = check_validation($arr);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $res = access::set_role($_REQUEST['role'], $_REQUEST['createby']);
+        if(is_numeric($res)){
+            send_msg(lang::$success, lang::$error);
+        }else{
+            send_msg(lang::$last_registered_data, lang::$error);
+        }
+        break;
+    case 'set_userrole':
+        $arr = array("userid", "roleid", "createby");
+        $valid_data = check_validation($arr);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $res = access::set_user_role($_REQUEST['userid'], $_REQUEST['roleid'], $_REQUEST['createby']);
+        if(is_numeric($res)){
+            send_msg(lang::$success, lang::$error);
+        }else{
+            send_msg(lang::$last_registered_data, lang::$error);
+        }
+        break;
+    case 'delete_role':
+        $valid_data = check_validation(array("id"));
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        if (access::delete_role($_REQUEST['id'])){
+            send_msg(lang::$success, lang::$message);
+        }else{
+            send_msg(lang::$invalid_data, lang::$failed);
+        }
+        break;
+    case 'delete_userrole':
+        $valid_data = check_validation(array("id"));
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        if (access::delete_user_role($_REQUEST['id'])){
+            send_msg(lang::$success, lang::$message);
+        }else{
+            send_msg(lang::$invalid_data, lang::$failed);
+        }
+        break;
+    case 'update_role':
+        $arr = array("id", "title", "menuid", "createby");
+        $valid_data = check_validation($arr);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        if(access::update_articles($_REQUEST['id'], $_REQUEST['pageid'], $_REQUEST['priority'], $_REQUEST['content'], $_REQUEST['metadata'], $_REQUEST['createby'])){
+            send_msg(lang::$success, lang::$message);
+        }else{
+            send_msg(lang::$invalid_data, lang::$failed);
+        }
+        break;
+    case 'update_userrole':
+        $arr = array("id", "title", "menuid", "createby");
+        $valid_data = check_validation($arr);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        break;
+
+    //_____________ receive sms
+    case 'receiveSms':
+        $arr = array("IMEI" , "number", "text" , "smsId" , "registerDate" );
+        $valid_data = check_validation($arr);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $device_id  = access::get_device_by_IMEI($_REQUEST["IMEI"]);
+      //  echo $device_id[0]["id"];
+        if(isset($device_id[0]["id"])){
+            $result =  access::set_sms($_REQUEST["number"] , $_REQUEST["text"], 0 ,$_REQUEST['smsId'],$_REQUEST['registerDate'], 0 , 2 , $device_id[0]["id"] , 1 ) ;
+            if($result > 0 && isset($result)){
+                send_msg(lang::$success, lang::$error);
+            }else{
+                send_msg(lang::$failed , lang::$error);
+            }
+        }else{
+                send_msg(lang::$invalid_device, lang::$error);
+        }
+
+        break;
+
+    //______________ get all sms
+
+    case 'get_sms':
+        $result = access::get_all_sms();
+        send_result($result);
+        break;
+    case 'get_sms_recive':
+        $result = access::get_sms_by_type(2);
+        send_result($result);
+        break;
+    case 'get_sms_send':
+        $result = access::get_sms_by_type(1);
+        send_result($result);
+        break;
+    case 'delete_sms':
+        $valid_data = check_validation(array("id"));
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        if (access::delete_sms($_REQUEST['id'])){
+            send_msg(lang::$success, lang::$message);
+        }else{
+            send_msg(lang::$invalid_data, lang::$failed);
+        }
+
+        break;
+
+    case 'get_sms_type_list':
+        $result = access::get_sms_type_list();
+        send_result($result);
+        break;
+
+    case 'get_sms_status_list':
+        $result = access::get_sms_status_list();
+        send_result($result);
+        break;
+
+    case 'set_sms_status':
+        $valid_data = check_validation(array("id","status"));
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        if (access::update_sms_status($_REQUEST['id'],$_REQUEST['status'])){
+            send_msg(lang::$success, lang::$message);
+        }else{
+            send_msg(lang::$invalid_data, lang::$failed);
+        }
+
+        break;
+
+    //_____________ get all request for device
+    case 'todo':
+        $arr = array("IMEI");
+        $valid_data = check_validation($arr);
+        if (!isset($valid_data['is_valid']) || $valid_data['is_valid'] == false) {
+            send_msg(lang::$invalid_data, lang::$error);
+            exit;
+        }
+        $device_id  = access::get_device_by_IMEI($_REQUEST["IMEI"]);
+        if(isset($device_id[0]['id'])) {
+            $result = access::get_sms_requste_by_device($device_id[0]['id']);
+            send_result($result);
+            exit;
+        }else{
+            send_msg(lang::$invalid_device, lang::$error);
+            exit;
+        }
+        break;
+
+    default:
+        echo "invalid act!!!";
+}
+
+function check_validation($field)
+{
+
+    $result['is_valid'] = true;
+
+    for ($i = 0; count($field) > $i; $i++) {
+        if (isset($_REQUEST[$field[$i]])) {
+            $result[$field[$i]] = true;
+        } else {
+            $result[$field[$i]] = false;
+            $result['is_valid'] = false;
+        }
+    }
+
+    return $result;
+
+}
+
+function send_msg($msg, $title,$type = "error", $btn = "")
+{
+    send_result(array('msg' => $msg, 'title' => $title, 'type'=>$type , 'btn'=>$btn ,  'act' => 'message' ));
+}
+
+function send_result($res)
+{
+    echo json_encode($res);
+}
---
+++ admin_panel/model/data_access/access.php
@@ -0,0 +1,224 @@
+<?php
+
+//try {
+require_once '../model/database/data.php';
+
+class access
+{
+
+    public static function set_role($role, $createBy){
+        return data::insertinto("`roles`", "`id`, `role`, `createby`, `creationdate`", "NULL, '$role', '$createBy', CURRENT_TIMESTAMP");
+    }
+
+    public static function get_role_by_id($id){
+        $data = data::selects("`roles`", "`id` = $id");
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_all_role(){
+        $data = data::selects("`roles`", "");
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function update_role($id, $role, $createBy){
+        return data::update("`roles`", "`role` = '$role', `createby` = '$createBy'", "`id` = $id");
+    }
+
+    public static function delete_role($id){
+        return data::delete("`roles`", "`id` = $id");
+    }
+
+    public static function set_user_role($userId, $roleId, $createBy){
+        return data::insertinto("`userroles`", "`userid`, `roleid`, `createby`, `creationdate`", "'$userId', '$roleId', '$createBy', CURRENT_TIMESTAMP");
+    }
+
+    public static function get_user_role_by_user_id($userId){
+        $data = data::selects("`userroles`", "`userid` = $userId");
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_all_user_role(){
+        $data = data::selects("`userroles`", "");
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function update_user_role($userId, $roleId, $createBy){
+        return data::update("`userroles`", "`userid` = '$userId', `roleid` = '$roleId', `createby` = '$createBy'", "`userid` = $userId");
+    }
+
+    public static function delete_user_role($id){
+        return data::delete("`userroles`", "`id` = $id");
+    }
+
+    public static function get_user_by_email_pass($email , $pass)
+    {
+        //
+        $data = data::selects('`users`', "`email` = '$email' AND  `password` ='$pass'" );
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function set_user($emial, $password , $avatar , $username)
+    {
+        return data::insertinto("`users`", "`ID`,`password`,`email`,`avatar`,`username`, `creationDate`", "NULL, '" . $password . "', '" . $emial . "', '" . $avatar . "', '" . $username . "', DATE_SUB( CURRENT_TIME(), INTERVAL 30 MINUTE)");
+    }
+
+    public static function edit_user($id,$username,$name,$sex,$address,$email,$cart,$sheba)
+    {
+        return data::update("`users`", "`username` = '$username'  , `name` = '$name' ,  `sex`= '$sex' ,  `address` = '$address' ,  `email` = '$email' ,  `cart` = '$cart' , `sheba` = '$sheba'","`id`='$id'");
+    }
+
+    public static function set_user_login($id)
+    {
+        return data::update("`users`", "`last_login` = DATE_SUB( CURRENT_TIME(), INTERVAL 30 MINUTE)","`id`='$id'");
+    }
+
+    public static function get_user_by_username_or_email($user)
+    {
+        //
+        $data = data::selects('`users`', "`email` like '%$user%' OR  `username` like '%$user%'" );
+
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_all_sms(){
+        $data = data::selects("`sms`", "");
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_sms_by_type($type){
+        $data = data::selects("`sms`", "`type` = $type");
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_sms_requste(){
+        $data = data::call('get_sms_requst_device');
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_sms_requste_by_device($device){
+        $data = data::call('get_sms_requst_device_by_device_id',$device);
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    public static function set_sms($number , $text , $reqnumber ,$smsId , $registerDate , $status , $type , $deviceid , $createdby)
+    {
+        return data::insertinto("`sms`", "`id`, `number`, `text`, `reqnumber`,`smsId`, `registerDate` , `status`, `type`, `deviceid`, `createdby`, `creationdate`", "NULL,'$number', '$text', $reqnumber, $smsId , $registerDate , $status , $type , $deviceid , $createdby , CURRENT_TIMESTAMP");
+    }
+
+    public static function update_sms($id, $number , $text, $reqnumber, $status, $type, $deviceid, $createBy , $smsId = null)
+    {
+        if ($smsId == null) {
+            return data::update("`sms`", "`number`= '$number',`text` = '$text', `reqnumber` = $reqnumber , `status` = $status, `type` = $type , `deviceid` = $deviceid , `createby` = '$createBy'", "`id` = $id");
+        }else{
+            return data::update("`sms`", "`number`= '$number',`text` = '$text', `reqnumber` = $reqnumber , `status` = $status, `type` = $type , `deviceid` = $deviceid , `createby` = '$createBy'", "`smsId` = $smsId");
+        }
+    }
+
+    public static function get_sms_type_list(){
+
+        $data = data::selects('`smstype`', "" );
+
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_sms_status_list(){
+
+        $data = data::selects('`smsStatus`', "" );
+
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function update_sms_status($id,$status){
+        return data::update("`sms`", "`status` = $status", "`id` = $id");
+    }
+
+    public static function delete_sms($id){
+        return data::delete("`sms`", "`id` = $id");
+    }
+
+    public static function get_all_device(){
+        $data = data::selects("`device`", "");
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function get_device_by_IMEI($IMEI)
+    {
+        //
+        $data = data::selects('`device`', "`IMEI` like '$IMEI'" );
+
+        if (count($data[0]) != 0) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    public static function set_device( $name, $IMEI , $createdby)
+    {
+        return data::insertinto("`device`", "`id`, `name`, `IMEI`, `createdby`, `creationdate`", "NULL,$name , $IMEI , $createdby , CURRENT_TIMESTAMP");
+    }
+
+    public static function update_device($id, $name , $IMEI , $createBy){
+        return data::update("`device`", "`name` =$name , `IMEI` =$IMEI , `createby` = '$createBy'", "`id` = $id");
+    }
+
+    public static function delete_device($id){
+        return data::delete("`device`", "`id` = $id");
+    }
+
+
+}
